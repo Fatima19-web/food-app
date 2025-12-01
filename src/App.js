4921aefea5a1ba24f4bb51dc4c0cf2fd4c17b826
fatima <faizamanzoor947@gmail.com>
2025-12-01 22:53:57 +0500

    first commit for food app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function MyInfo() {
+  return (
+    <div>
+      <h2><b>Introduction</b></h2>
+      <p>
+        Hi! My name is <strong>Fatima</strong>. I am currently studying in 
+        <strong> Class 10</strong>. I am 15 years old .I am currently doing a course of web and app development in saylani welfare.
+      </p>
+
+      <p>
+        I have a creative personality and love expressing myself through 
+        <strong> sketching</strong> and <strong>writing</strong>.
+        I enjoy turning ideas into beautiful art and meaningful stories.
+      </p>
+
+      <p>
+        I also love <strong>traveling</strong> and exploring new places. 
+        Every new experience teaches me something new about the world
+        and about myself. 🌍✨
+        I believe learning and creativity make life exciting, and I hope
+        to continue growing, improving, and discovering new passions along the way!
+      </p>
+    </div>
+  );
+}
+
+export default MyInfo;
